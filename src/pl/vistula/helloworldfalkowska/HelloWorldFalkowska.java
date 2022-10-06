c9deea9e1255753786db1654e2941342770b77ee
PFalkowska <pfalkow1@stu.vistula.edu.pl>
2022-10-06 16:19:33 +0200

    commit2

--- src/pl/vistula/helloworldfalkowska/HelloWorldFalkowska.java
+++ src/pl/vistula/helloworldfalkowska/HelloWorldFalkowska.java
@@ -3,5 +3,7 @@ package pl.vistula.helloworldfalkowska;
 public class HelloWorldFalkowska {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Falkowska");
+
+        System.out.println("Hello 1");
     }
 }
